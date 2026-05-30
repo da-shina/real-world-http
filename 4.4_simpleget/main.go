06d89da693da09e957bc72371a59bc4d94eded39
da-shina <139605659+da-shina@users.noreply.github.com>
2026-05-30 19:52:15 +0900

    クエリ文字列の送信機能を追加し、main関数を分割
    - .gitignoreに.DS_Storeを追加
    - 4.4_simpleget/main.go: main_4_4()とmain()に分割
    - GETクエリの送信機能をmain()に追加

--- 4.4_simpleget/main.go
+++ 4.4_simpleget/main.go
@@ -4,21 +4,39 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
-func main() {
+// 4.4 GETメソッドの送信とコンテンツ、ステータス、フィールドの送信
+func main_4_4() {
+	// GETメソッドの送信
 	resp, err := http.Get("http://localhost:18888")
 	if err != nil {
 		panic(err)
 	}
+
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
+	//  BODYコンテンツの表示
 	log.Println(string(body))
+	//  ステータスの表示
 	log.Println("Status:", resp.Status)
 	log.Println("StatusCode:", resp.StatusCode)
+	//  フィールドの表示
 	log.Println("Fields:", resp.Header)
 	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
+}
+
+// 4.5 GETメソッド・クエリーの送信
+func main() {
+	values := url.Values{
+		"query": {"hello world"},
+	}
+	resp, _ := http.Get("http://localhost:18888" + "?" + values.Encode())
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	log.Println(string(body))
 }
